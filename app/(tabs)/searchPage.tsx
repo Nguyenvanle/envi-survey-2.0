d79f7d6122f8aa7f13a880ed94ee5d36dc511513
Tú Lê <leduonganhtu0102@gmail.com>
2024-03-30 14:14:28 +0700

    0.4
    Backup

--- app/(tabs)/searchPage.tsx
+++
@@ -1,7 +0,0 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { container } from "@/constants/Styles";
-
-export default function searchPage() {
-  return <View style={container.root}></View>;
-}
