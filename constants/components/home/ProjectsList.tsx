a40e641dfae182789e5545f9c2110c648a6f6601
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-04-12 15:02:45 +0700

    fix 2 icon project list

--- constants/components/home/ProjectsList.tsx
+++ constants/components/home/ProjectsList.tsx
@@ -1,7 +1,7 @@
 import Colors from "@/constants/Colors";
 import { defaultStyles } from "@/constants/Styles";
 import { getInitials } from "@/constants/logic/getInitials";
-import { MaterialCommunityIcons, SimpleLineIcons } from "@expo/vector-icons";
+import { Feather, SimpleLineIcons } from "@expo/vector-icons";
 import React from "react";
 import { Text, View } from "react-native";
 import { CustomAvatar } from "./CustomAvatar";
@@ -13,12 +13,12 @@ export function ProjectsList(props: {
     position: string;
     projectsTaken: number;
     projectsCompleted: number;
-  },
+  };
   projects: {
     name: string;
-    manage: string,
-    startDate: string,
-    endDate: string,
+    manage: string;
+    startDate: string;
+    endDate: string;
   };
 }) {
   return (
@@ -68,16 +68,6 @@ export function ProjectsList(props: {
             gap: 4,
           }}
         >
-          <Text
-            style={{
-              ...HomeStyles.text,
-              color: Colors.muted,
-              fontSize: 12,
-              textAlign: "right",
-            }}
-          >
-            80.5%
-          </Text>
           <SimpleLineIcons name="chart" size={20} color={Colors.muted} />
         </View>
       </View>
@@ -86,109 +76,19 @@ export function ProjectsList(props: {
 
       <View style={HomeStyles.projectContainer}>
         <View style={HomeStyles.divideContainer}>
-          <MaterialCommunityIcons
-            name="folder-check-outline"
-            size={16}
-            color={Colors.gray}
-          />
+          <Feather name="clock" size={16} color={Colors.deepBlue} />
 
-          <Text style={HomeStyles.miniText}>{props.projects.startDate}</Text>
-        </View>
-
-        <View style={HomeStyles.divideContainer}>
-          <MaterialCommunityIcons
-            name="folder-check-outline"
-            size={16}
-            color={Colors.gray}
-          />
-
-          <Text style={HomeStyles.miniText}>{props.projects.endDate}</Text>
-        </View>
-      </View>
-    </View>
-  );
-  return (
-    <View
-      style={{
-        ...HomeStyles.userInfoContainer,
-        backgroundColor: Colors.blueWhite,
-      }}
-    >
-      <View style={HomeStyles.infoContainer}>
-        <View
-          style={{
-            maxHeight: 47,
-            minHeight: 17,
-            height: "auto",
-          }}
-        >
-          {CustomAvatar(getInitials, props.user)}
-        </View>
-
-        <View style={{ ...HomeStyles.namePosition }}>
-          <Text
-            style={{
-              ...HomeStyles.textPrimary,
-              color: Colors.gray,
-              fontSize: 14,
-              lineHeight: 18,
-            }}
-          >
-            {props.projects.name}
-          </Text>
-          <Text
-            style={{
-              ...HomeStyles.text,
-              color: Colors.muted,
-              fontSize: 12,
-            }}
-          >
-            {props.user.name}
+          <Text style={{ ...HomeStyles.miniText, color: Colors.deepBlue }}>
+            {props.projects.startDate}
           </Text>
         </View>
 
-        <View
-          style={{
-            ...HomeStyles.icon,
-            flexDirection: "row-reverse",
-            gap: 4,
-          }}
-        >
-          <Text
-            style={{
-              ...HomeStyles.text,
-              color: Colors.muted,
-              fontSize: 12,
-              textAlign: "right",
-            }}
-          >
-            80.5%
+        <View style={HomeStyles.divideContainer}>
+          <Feather name="clock" size={16} color={Colors.red} />
+
+          <Text style={{ ...HomeStyles.miniText, color: Colors.red }}>
+            {props.projects.endDate}
           </Text>
-          <SimpleLineIcons name="chart" size={20} color={Colors.muted} />
-        </View>
-      </View>
-
-      <View style={defaultStyles.separator}></View>
-
-      <View style={HomeStyles.projectContainer}>
-        <View style={HomeStyles.divideContainer}>
-          <MaterialCommunityIcons
-            name="folder-check-outline"
-            size={16}
-            color={Colors.gray}
-          />
-
-          <Text style={HomeStyles.miniText}>this.props.first</Text>
-        </View>
-
-        <View style={HomeStyles.divideContainer}>
-          <MaterialCommunityIcons
-            name="folder-check-outline"
-            size={16}
-            color={Colors.gray}
-          />
-
-          <Text style={HomeStyles.miniText}>6 dự án</Text>
         </View>
       </View>
     </View>
