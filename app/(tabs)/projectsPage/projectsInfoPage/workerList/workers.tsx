957c0c93ac590838ee73f0218887e9c303851bc9
khachhoangthienn <tiendat29082003@gmail.com>
2024-04-15 13:17:41 +0700

    final

---
+++ app/(tabs)/projectsPage/projectsInfoPage/workerList/workers.tsx
@@ -0,0 +1,39 @@
+import { container, text } from "@/constants/Styles";
+
+import { listWorkerFireBase } from "@/constants/logic/projectFirebase";
+import { useGlobalSearchParams } from "expo-router";
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { WorkerList } from './../../../../../constants/components/home/WorkersList';
+
+export default function listEmployees() {
+  const {projectID} = useGlobalSearchParams();
+
+  const {
+    listWorkerMap,
+    isLoading,
+  } = listWorkerFireBase(projectID);
+  
+  const workers = Array.from(listWorkerMap.entries()).map(
+    ([workerUid, workerData]) => ({
+      uid: workerUid,
+      name: workerData.fullName,
+    })
+  );
+  if(isLoading) return;
+  return (
+    <SafeAreaProvider >
+      <ScrollView>
+        {/* listEmployees */}
+        <View style={{ ...container.root, gap: 15 , alignItems: "flex-start"}}>
+<Text style={{...text.headerPrimary, fontSize: 20}}>Thành Viên</Text>
+        <WorkerList
+                workers = {workers.map((worker) => ({ ...worker}))}
+              ></WorkerList>
+
+        </View>
+      </ScrollView>
+    </SafeAreaProvider>
+  );
+}
