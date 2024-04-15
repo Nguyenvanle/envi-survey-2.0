1d48b15b25a75b7531eb999dfd84c09db6a399c7
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-04-15 10:10:35 +0700

    sua giao dien xem du an

--- app/(tabs)/projectsPage/projectsInfoPage/sampList/[sampID].tsx
+++ app/(tabs)/projectsPage/projectsInfoPage/sampList/[sampID].tsx
@@ -42,7 +42,6 @@ export default function SamplingPeriod() {
             style={{
               ...styles.body,
               gap: 10,
-              backgroundColor: "red",
               padding: 0,
             }}
           >
@@ -50,7 +49,6 @@ export default function SamplingPeriod() {
               style={{
                 ...styles.subFrame,
                 paddingHorizontal: 30,
-                backgroundColor: "blue",
                 justifyContent: "flex-start",
               }}
             >
